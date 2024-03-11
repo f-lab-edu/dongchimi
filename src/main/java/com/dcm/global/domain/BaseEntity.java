ce1e830fb5028dab845b8af7cda1742ada018a43
scnoh <stc9606@naver.com>
2024-03-11 22:05:29 +0900

    setting. project 환경 구성

---
+++ src/main/java/com/dcm/global/domain/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.dcm.global.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updateDate;
+
+}

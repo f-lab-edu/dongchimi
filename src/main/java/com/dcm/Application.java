4bee698e67fa87aaf2d87b108452fa8fb52b8fb1
scnoh <stc9606@naver.com>
2024-03-23 16:38:01 +0900

    fix. 안 쓰는 Bean 제거

--- src/main/java/com/dcm/Application.java
+++ src/main/java/com/dcm/Application.java
@@ -2,8 +2,6 @@ package com.dcm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
@@ -12,9 +10,5 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
 }

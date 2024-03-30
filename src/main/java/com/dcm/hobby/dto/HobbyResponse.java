8ca9ac124cf341450a3ccbd57ccb9829211777f7
scnoh <stc9606@naver.com>
2024-03-30 21:45:28 +0900

    feat. hobby 조회 시 상세 목록도 추가

--- src/main/java/com/dcm/hobby/dto/HobbyResponse.java
+++ src/main/java/com/dcm/hobby/dto/HobbyResponse.java
@@ -1,18 +1,22 @@
 package com.dcm.hobby.dto;
 
 import com.dcm.hobby.domain.Hobby;
+import com.dcm.hobby_detail.dto.HobbyDetailResponse;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public record HobbyResponse(Long hobbyId, String hobbyName, String useYn) {
+public record HobbyResponse(Long hobbyId, String hobbyName, String useYn, List<HobbyDetailResponse> hobbyDetails) {
 
-    public static HobbyResponse of (Hobby hobby) {
-        return new HobbyResponse(hobby.getHobbyId(), hobby.getHobbyName(), hobby.getUseYn());
+    public static HobbyResponse of(Hobby hobby) {
+        List<HobbyDetailResponse> hobbyDetailResponses = hobby.getHobbyDetails().stream()
+                .map(HobbyDetailResponse::of)
+                .collect(Collectors.toList());
+        return new HobbyResponse(hobby.getHobbyId(), hobby.getHobbyName(), hobby.getUseYn(), hobbyDetailResponses);
     }
 
 
-    public static List<HobbyResponse> of (List<Hobby> hobbyList) {
+    public static List<HobbyResponse> of(List<Hobby> hobbyList) {
         return hobbyList.stream()
                 .map(HobbyResponse::of)
                 .collect(Collectors.toList());

5797b03c75b97e0595c741a21b75d9bfd3497b56
scnoh <stc9606@naver.com>
2024-04-21 12:02:42 +0900

    [feat] webflux websocket 기능 구현
    1. partyId 별 채팅방 생성 및 사용자 세션 관리
    2. 나 자신의 채팅은 나에게 다시 전달되지 않아야 한다.

--- dongchimi-reactive/src/main/java/dcm/chat/handler/ChatWebSocketHandler.java
+++ dongchimi-reactive/src/main/java/dcm/chat/handler/ChatWebSocketHandler.java
@@ -1,40 +1,60 @@
 package dcm.chat.handler;
 
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 public class ChatWebSocketHandler implements WebSocketHandler {
 
-    private final Map<String, WebSocketSession> partyChatRooms = new ConcurrentHashMap<>();
+    private final Map<String, Set<WebSocketSession>> partyChatRooms = new ConcurrentHashMap<>();
 
     @Override
     public Mono<Void> handle(WebSocketSession session) {
         String partyId = getPartIdFromQuery(session);
-
-        partyChatRooms.put(partyId, session);
+        Set<WebSocketSession> chatSession = getChatSession(partyId, session);
 
         return session.receive()
-                .map(WebSocketMessage::getPayloadAsText)
-                .flatMap(message -> sendMessageToSession(partyId, message))
-                .then()
-                .doFinally(signal -> partyChatRooms.remove(partyId));
+            .map(WebSocketMessage::getPayloadAsText)
+            .flatMap(message -> sendMessageToSession(chatSession, session, message))
+            .doFinally(signalType -> removeChatSession(partyId, session))
+            .then();
     }
 
-    private Mono<Void> sendMessageToSession(String partId, String message) {
-        WebSocketSession session = partyChatRooms.get(partId);
-        if (session != null) {
-            return session.send(Mono.just(session.textMessage(message)));
+    private Set<WebSocketSession> getChatSession(String partyId, WebSocketSession session) {
+        Set<WebSocketSession> sessions = partyChatRooms.computeIfAbsent(partyId, s -> new CopyOnWriteArraySet<>());
+        sessions.add(session);
+        return sessions;
+    }
+
+    private void removeChatSession(String partyId, WebSocketSession session) {
+        Set<WebSocketSession> sessions = partyChatRooms.get(partyId);
+        if (sessions != null) {
+            sessions.remove(session);
+            if (sessions.isEmpty()) {
+                partyChatRooms.remove(partyId);
+            }
         }
-        return Mono.empty();
+    }
+
+    private Mono<Void> sendMessageToSession(Set<WebSocketSession> chatSession, WebSocketSession senderSession, String message) {
+        return Mono.when(chatSession.stream()
+                .filter(WebSocketSession::isOpen)
+                .filter(session -> !session.getId().equals(senderSession.getId()))
+                .map(session -> session.send(Mono.just(session.textMessage(message))))
+                .toArray(Mono[]::new));
     }
 
     private String getPartIdFromQuery(WebSocketSession session) {
         String query = session.getHandshakeInfo().getUri().getQuery();
-        return query.split("partyId=")[1];
+        if (ObjectUtils.isEmpty(query))
+            throw new IllegalArgumentException("전달된 파라미터가 없습니다.");
+        return query.split("&")[0].split("partyId=")[1];
     }
 }
